4b1f5ed4a8964fad67a9b8fc745b96f146e84fa1
wanggege919 <1914161290@qq.com>
2018-10-18 11:50:02 +0800

    改变线条粗细,添加下载按钮

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var yyy = document.getElementById('xxx');
 var context = yyy.getContext('2d');
+var lineWidth = 5;
 
 autoSetCanvasSize(yyy);
 
@@ -8,15 +9,58 @@ listenToUser(yyy);
 
 
 var eraserEnabled = false;
-eraser.onclick = function () {
-    eraserEnabled = true;
-    action.className = "action x"
-}
-brush.onclick = function () {
+pen.onclick = function(){
     eraserEnabled = false;
-    action.className = "action"
+    pen.classList.add('active');
+    eraser.classList.remove('active');
+}
+eraser.onclick = function(){
+    eraserEnabled = true;
+    eraser.classList.add('active');
+    pen.classList.remove('active');
+}
+clear.onclick = function(){
+    context.clearRect(0,0,yyy.width,yyy.height)
+}
+save.onclick = function(){
+    var url = yyy.toDataURL("img/png")
+    var a = document.createElement('a')
+    document.body.appendChild(a)
+    a.href = url
+    a.download = '我的作品'
+    a.target = '_blank'
+    a.click()
 }
 
+
+red.onclick = function(){
+    context.fillStyle = 'red'
+    context.strokeStyle = 'red'
+    red.classList.add('active')
+    green.classList.remove('active')
+    blue.classList.remove('active')
+
+}
+green.onclick = function(){
+    context.fillStyle = 'green'
+    context.strokeStyle = 'green'
+    red.classList.remove('active')
+    green.classList.add('active')
+    blue.classList.remove('active')
+}
+blue.onclick = function(){
+    context.fillStyle = 'blue'
+    context.strokeStyle = 'blue'
+    red.classList.remove('active')
+    green.classList.remove('active')
+    blue.classList.add('active')
+}
+thin.onclick = function(){
+    lineWidth = 3;
+}
+thick.onclick = function(){
+    lineWidth = 6;
+}
 /*******/
 
 function autoSetCanvasSize(canvas) {
@@ -44,7 +88,7 @@ function drawCircle(x, y, radius) {
 function drawLine(x1, y1, x2, y2) {
     context.beginPath();
     context.moveTo(x1, y1);
-    context.lineWidth = 5;
+    context.lineWidth = lineWidth;
     context.lineTo(x2, y2);
     context.stroke();
     context.closePath();
